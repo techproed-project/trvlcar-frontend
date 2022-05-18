71e062ae22c8f062d692bb7bda09040b1439da22
ZiyaS <45655749+ziya3435@users.noreply.github.com>
2022-05-18 21:25:02 +0300

    v7

---
+++ src/components/users/common/testimonals/testimonals.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Col, Container } from 'react-bootstrap';
+import "./testimonals.css";
+
+const Testimonals = () => {
+  return (
+    <section className="testimonals">
+        <Container>
+            <Row>
+            <Col md={6}>
+                
+            </Col>
+            <Col md={6}>
+
+            </Col>
+            </Row>
+        </Container>
+    </section>
+  )
+}
+
+export default Testimonals
