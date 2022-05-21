ee8db6083d492498d04d8270216efdf60364bc1f
ZiyaS <45655749+ziya3435@users.noreply.github.com>
2022-05-21 22:38:59 +0300

    v16

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { ToastContainer } from "react-toastify";
 import { getUser } from "./api/user-service";
+import LoadingPage from "./pages/users/LoadingPage";
 import CustomRoutes from "./router/custom-routes";
-import { StoreProvider, useStore } from "./store";
+import { useStore } from "./store";
 import { loginSuccess } from "./store/user/userActions";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
   const {dispatchUser} = useStore();
 
   const loadData = async () =>  { 
     try {
       const resp = await getUser();
       dispatchUser(loginSuccess(resp.data));
+      setLoading(false);
       
     } catch (err) {
       console.log(err);
+      setLoading(false);
     }
   }
 
@@ -23,8 +27,9 @@ const App = () => {
   }, []);
   
 
- 
-
+  if(loading) 
+    return(<LoadingPage/>)
+  else
   return (
     <>
       <CustomRoutes/>
--- src/components/users/auth/register-form.js
+++ src/components/users/auth/register-form.js
@@ -1,8 +1,45 @@
 import React from "react";
 import { Button, Form } from "react-bootstrap";
 import Spacer from "../common/spacer/spacer";
-
+import * as Yup from "yup";
+import MaskedInput from "react-maskedinput";
 const RegisterForm = () => {
+
+  const initialValues = {
+    firstName:"",
+    lastName:"",
+    phoneNumber:"",
+    address:"",
+    zipCode:"",
+    email:"",
+    password:"",
+    confirmPassword:""
+  }
+
+  const validationSchema = Yup.object({
+    firstName: Yup.string().required("Please enter your first name"),
+    lastName: Yup.string().required("Please enter your last name"),
+    phoneNumber: Yup.string().required("Please enter your phone number"),
+    address: Yup.string().required("Please enter your address"),
+    zipCode: Yup.string().required("Please enter your zip code"),
+    password: Yup.string().required("Please enter your password")
+    .min(8, "Must be at least 8 characters")
+    .matches(/[a-z]+/,"One lowercase character")
+    .matches(/[A-Z]+/,"One uppercase character")
+    .matches(/[@$!%*#?&]+/,"One special character")
+    .matches(/\d+/,"One number"),
+    confirmPassword: Yup.string().required("Please re-enter your password")
+    .oneOf([Yup.ref("password")],"Password fields doesn't match")
+  }); 
+
+  const onSubmit = (values) => {
+
+  }
+
+  
+
+
+
   return (
     <Form>
       <Form.Group className="mb-3">
@@ -15,7 +52,8 @@ const RegisterForm = () => {
       </Form.Group>
       <Form.Group className="mb-3">
         <Form.Label>Phone Number</Form.Label>
-        <Form.Control type="text" />
+        <Form.Control type="text" as={MaskedInput} 
+        mask="(111) 111-1111" />
       </Form.Group>
       <Form.Group className="mb-3">
         <Form.Label>Address</Form.Label>
