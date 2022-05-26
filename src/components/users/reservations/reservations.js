5b966a62bd83e069288066375044560958ccd091
ZiyaS <45655749+ziya3435@users.noreply.github.com>
2022-05-26 22:22:50 +0300

    v30

--- src/components/users/reservations/reservations.js
+++ src/components/users/reservations/reservations.js
@@ -1,24 +1,33 @@
+import moment from "moment";
 import React, { useEffect, useState } from "react";
-import { Container, Table } from "react-bootstrap";
+import { Container, Spinner, Table } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import { getReservations } from "../../../api/reservation-service";
 
 const Reservations = () => {
-    const [loading, setLoading] = useState(true);
-    const [reservations, setReservations] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [reservations, setReservations] = useState([]);
+  const navigate = useNavigate();
 
-    useEffect( async () => {
-        try{
-            const resp = await getReservations();
-            setReservations(resp.data);
-        }
-        catch(err){
-            console.log(err);
-        }
-        finally{
-            setLoading(false);
-        }
-    }, []);
-    
+  const loadData = async () => {
+    try {
+      const resp = await getReservations();
+      console.log(resp.data);
+      setReservations(resp.data);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleClick = (id) => {
+    navigate(`/user/reservations/${id}`);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
 
   return (
     <Container>
@@ -33,12 +42,29 @@ const Reservations = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
+          {loading && (
+            <tr>
+              <td colSpan={5}>
+                <Spinner animation="border" size="sm" /> Loading...
+              </td>
+            </tr>
+          )}
+
+          {reservations.map((reservation, index) => (
+            <tr key={index} onClick={ ()=>handleClick(reservation.id)}>
+              <td>{index + 1}</td>
+              <td>{reservation.car.model}</td>
+              <td>{moment(reservation.pickUpTime).format("llll")}</td>
+              <td>{moment(reservation.dropOfTime).format("llll")}</td>
+              <td>{reservation.status}</td>
+            </tr>
+          ))}
+
+          {reservations.length <= 0 && (
+            <tr className="table-warning">
+              <td colSpan={5}>No reservation</td>
+            </tr>
+          )}
         </tbody>
       </Table>
     </Container>
