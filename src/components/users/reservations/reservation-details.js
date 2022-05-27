8cf7f89f113726834d2b5cb7caad5ec0154edb86
ZiyaS <45655749+ziya3435@users.noreply.github.com>
2022-05-27 19:23:23 +0300

    v32

--- src/components/users/reservations/reservation-details.js
+++ src/components/users/reservations/reservation-details.js
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { Accordion, Col, Container, Image, Row, Table } from "react-bootstrap";
+import {
+  Accordion,
+  Button,
+  Col,
+  Container,
+  Image,
+  Row,
+  Table,
+} from "react-bootstrap";
 import { getReservation } from "../../../api/reservation-service";
+import { FiCheck, FiCrosshair, FiArrowLeft, FiX } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
 
 const ReservationDetails = ({ reservationId }) => {
   const [reservation, setReservation] = useState({});
   const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
   const loadData = async () => {
     try {
@@ -32,6 +43,13 @@ const ReservationDetails = ({ reservationId }) => {
               src={`${process.env.REACT_APP_API_URL}/files/display/${reservation.car.image[0]}`}
               className="img-fluid"
             />
+            <Button
+              variant="primary"
+              className="mt-3"
+              onClick={() => navigate(-1)}
+            >
+              <FiArrowLeft /> Back to reservations
+            </Button>
           </Col>
           <Col md={6}>
             <Accordion defaultActiveKey="0">
@@ -42,7 +60,7 @@ const ReservationDetails = ({ reservationId }) => {
                     <thead>
                       <tr>
                         <th colSpan={2}>
-                            <h3>${reservation.totalPrice}</h3>
+                          <h3>${reservation.totalPrice}</h3>
                         </th>
                       </tr>
                     </thead>
@@ -74,7 +92,48 @@ const ReservationDetails = ({ reservationId }) => {
               <Accordion.Item eventKey="1">
                 <Accordion.Header>Car Details</Accordion.Header>
                 <Accordion.Body>
-                    
+                  <Table striped bordered hover>
+                    <tbody>
+                      <tr>
+                        <td>Model</td>
+                        <td>{reservation.car.model}</td>
+                      </tr>
+                      <tr>
+                        <td>Doors</td>
+                        <td>{reservation.car.doors}</td>
+                      </tr>
+                      <tr>
+                        <td>Seats</td>
+                        <td>{reservation.car.seats}</td>
+                      </tr>
+                      <tr>
+                        <td>Luggage</td>
+                        <td>{reservation.car.luggage}</td>
+                      </tr>
+                      <tr>
+                        <td>Transmission</td>
+                        <td>{reservation.car.transmission}</td>
+                      </tr>
+                      <tr>
+                        <td>Air Conditioning</td>
+                        <td>
+                          {reservation.car.airConditioning ? (
+                            <FiCheck />
+                          ) : (
+                            <FiCrosshair />
+                          )}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>Fuel Type</td>
+                        <td>{reservation.car.fuelType}</td>
+                      </tr>
+                      <tr>
+                        <td>Age</td>
+                        <td>{reservation.car.age}</td>
+                      </tr>
+                    </tbody>
+                  </Table>
                 </Accordion.Body>
               </Accordion.Item>
             </Accordion>
